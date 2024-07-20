31cd6ee4f0beef72c001545fe6eda57d218c773e
pabloPNC <pablo.navarrocarpio@gmail.com>
2024-07-20 19:28:15 +0200

    Created tests for tp_auc.R

--- tests/testthat/test-tp_auc.R
+++ tests/testthat/test-tp_auc.R
@@ -1,17 +1,28 @@
 data <- tibble::tibble(readRDS(test_path("fixtures", "roc_data.rds")))
 response <- "disease"
 predictor <- "ENSG00000000003.15"
-lower_fpr <- 0
-upper_fpr <- 0.1
 
 test_that("tp_auc == TpAUC.function", {
-    skip()
-    print(
-        tp_auc(
-            response = data[[response]],
-            predictor = data[[predictor]],
-            lower_fpr = lower_fpr,
-            upper_fpr = upper_fpr
-        )
+    lower_threshold <- 0.4
+    upper_threshold <- 0.49
+
+    actual_tpauc <- tp_auc(
+        data = NULL,
+        response = data[[response]],
+        predictor = data[[predictor]],
+        lower_fpr = lower_threshold,
+        upper_fpr = upper_threshold
+    )
+
+    expected_tpauc <- TpAUC.function(
+        data[[response]],
+        data[[predictor]],
+        lower.fp = lower_threshold,
+        upper.fp = upper_threshold
+    )
+
+    expect_equal(
+        actual_tpauc,
+        expected_tpauc
     )
 })
