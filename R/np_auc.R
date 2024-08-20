b64540140e1949ccfde8d61b4763e6a91e940665
pabloPNC <pablo.navarrocarpio@gmail.com>
2024-08-20 05:34:45 +0200

    Initial version of np_auc

---
+++ R/np_auc.R
@@ -0,0 +1,44 @@
+#' @export
+np_auc <- function(
+            data = NULL,
+            response,
+            predictor,
+            lower_tpr) {
+
+    if (!is.null(data)) {
+        tpr_fpr <- data %>% roc_points({{ response }}, {{ predictor }})
+        tpr <- tpr_fpr %>% pull(tpr)
+        fpr <- tpr_fpr %>% pull(fpr)
+    } else {
+        tpr_fpr <- roc_points(NULL, response, predictor)
+        tpr <- tpr_fpr[["tpr"]]
+        fpr <- tpr_fpr[["fpr"]]
+    }
+
+    partial_tpr_fpr <- calc_partial_roc_points(
+        tpr = tpr,
+        fpr = fpr,
+        lower_threshold = lower_tpr,
+        upper_threshold = 1,
+        ratio = "tpr",
+        sort = TRUE,
+        include_thresholds = TRUE
+    )
+
+    partial_tpr <- partial_tpr_fpr[["partial_tpr"]]
+    partial_fpr <- partial_tpr_fpr[["partial_fpr"]]
+
+    pauc <- sum(
+        diff(partial_tpr) *
+            apply(
+                cbind(
+                    1 - partial_fpr[-1],
+                    1 - partial_fpr[-length(partial_tpr)]
+                ),
+                1,
+                mean
+            )
+    )
+
+    npauc <- pauc/(1 - lower_tpr)
+}
