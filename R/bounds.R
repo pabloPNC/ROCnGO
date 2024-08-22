5274995f605f6e393809164d7dfb127ee6b5c08a
pabloPNC <pablo.navarrocarpio@gmail.com>
2024-08-22 22:42:19 +0200

    Created lower bounds of FpAUC

--- R/bounds.R
+++ R/bounds.R
@@ -84,3 +84,107 @@ calc_fpr_bounds <- function(
         )
     )
 }
+
+calc_tpr_upper_bound <- function(
+        partial_fpr,
+        partial_tpr) {
+    return(
+        (1 - partial_fpr[1])*(1 - partial_tpr[1])
+    )
+}
+
+calc_tpr_concave_lower_bound <- function(
+        partial_fpr,
+        partial_tpr) {
+    return(
+        (1/2)*(1 - partial_fpr[1])*(1 - partial_tpr[1])
+    )
+}
+
+calc_tpr_partial_concave_lower_bound <- function(
+        partial_fpr,
+        partial_tpr) {
+    return(
+        (1/2)*(1 - partial_tpr[1])^2
+    )
+}
+
+calc_tpr_upper_hook_lower_bound <- function(
+        partial_fpr,
+        partial_tpr) {
+    return(0)
+}
+
+calc_nlr <- function(
+        partial_fpr,
+        partial_tpr) {
+    partial_fpr <- partial_fpr[-length(partial_fpr)]
+    partial_tpr <- partial_tpr[-length(partial_tpr)]
+    (1 - partial_tpr)/(1 - partial_fpr)
+}
+
+is_concave_tpr <- function(
+        partial_nlr) {
+    partial_nlr_0 <- partial_nlr[1]
+    all(partial_nlr <= partial_nlr_0) & (is.finite(partial_nlr_0))
+}
+
+is_partially_concave_tpr <- function(
+        partial_nlr) {
+    all(partial_nlr <= 1)
+}
+
+calc_tpr_curve_shape <- function(
+        partial_fpr,
+        partial_tpr) {
+    partial_nlr <- calc_nlr(partial_fpr, partial_tpr)
+    if (is_concave_tpr(partial_nlr)) {
+        curve_shape <- "Proper"
+    } else if (is_partially_concave_tpr(partial_nlr)) {
+        curve_shape <- "Partially proper"
+    } else {
+        curve_shape <- "Improper"
+    }
+    return(curve_shape)
+}
+
+calc_tpr_lower_bound <- function(
+        partial_fpr,
+        partial_tpr) {
+    curve_shape <- calc_tpr_curve_shape(
+        partial_fpr = partial_fpr,
+        partial_tpr = partial_tpr
+    )
+    if (curve_shape == "Proper") {
+        lower_bound <- calc_tpr_concave_lower_bound(
+            partial_fpr = partial_fpr,
+            partial_tpr = partial_tpr
+        )
+    } else if (curve_shape == "Partially proper") {
+        lower_bound <- calc_tpr_partial_concave_lower_bound(
+            partial_fpr = partial_fpr,
+            partial_tpr = partial_tpr
+        )
+    } else if (curve_shape == "Improper") {
+        lower_bound <- calc_tpr_upper_hook_lower_bound(
+            partial_tpr = partial_tpr,
+            partial_fpr = partial_fpr
+        )
+    }
+    return(lower_bound)
+}
+
+calc_tpr_bounds <- function(
+        partial_fpr,
+        partial_tpr) {
+    list(
+        upper_bound = calc_tpr_upper_bound(
+            partial_fpr,
+            partial_tpr
+        ),
+        lower_bound = calc_tpr_lower_bound(
+            partial_fpr,
+            partial_tpr
+        )
+    )
+}
