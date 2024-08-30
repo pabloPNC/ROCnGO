d439677b232e49b5be041941ce4ea87ab32e43c8
pabloPNC <pablo.navarrocarpio@gmail.com>
2024-08-30 21:36:17 +0200

    Implemented add_partial_* functions

--- R/plot_roc.R
+++ R/plot_roc.R
@@ -219,21 +219,242 @@ add_threshold_line <- function(
     }
 }
 
+#' @importFrom dplyr filter
+#' @importFrom ggplot2 aes
+add_partial_roc_from_ratios_fpr <- function(
+        data,
+        fpr,
+        tpr,
+        threshold,
+        geom) {
+    geom(
+        data = data %>% filter( {{fpr}} <= threshold),
+        mapping = aes(
+            x = {{ fpr }},
+            y = {{ tpr }}
+        ),
+        size = 0.8
+    )
+}
 
+add_partial_roc_from_ratios_tpr <- function(
+        data,
+        fpr,
+        tpr,
+        threshold,
+        geom) {
+    geom(
+        data = data %>% filter( {{tpr}} >= threshold),
+        mapping = aes(
+            x = {{ fpr }},
+            y = {{ tpr }}
+        ),
+        size = 0.8
+    )
+}
 
-# FIX here ----------------------------------------------------------------
+add_partial_roc_from_ratios <- function(
+        data,
+        fpr,
+        tpr,
+        ratio,
+        threshold,
+        geom) {
+    if (ratio == "tpr") {
+        add_partial_roc_from_ratios_tpr(
+            data,
+            {{ fpr }},
+            {{ tpr }},
+            threshold,
+            geom
+        )
+    } else if (ratio == "fpr") {
+        add_partial_roc_from_ratios_fpr(
+            data,
+            {{ fpr }},
+            {{ tpr }},
+            threshold,
+            geom
+        )
+    }
+}
 
+#' @importFrom rlang as_name enquo
+#' @importFrom dplyr filter
+#' @importFrom ggplot2 aes
+add_partial_roc_from_predictor_tpr <- function(
+        data,
+        response,
+        predictor,
+        threshold,
+        geom) {
+    predictor_name <- as_name(enquo(predictor))
+    if (is.null(data)) {
+        geom(
+            data = . %>%
+                roc_points({{ response }}, {{ predictor }}) %>%
+                filter(.data[["tpr"]] >= threshold),
+            mapping = aes(
+                x = .data[["fpr"]],
+                y = .data[["tpr"]],
+                color = predictor_name
+            ),
+            size = 0.8
+        )
+    } else {
+        geom(
+            data = roc_points(data, {{ response }}, {{ predictor }}) %>%
+                filter(.data[["tpr"]] >= threshold),
+            mapping = aes(
+                x = .data[["fpr"]],
+                y = .data[["tpr"]],
+                color = predictor_name
+            ),
+            size = 0.8
+        )
+    }
+}
 
+#' @importFrom rlang as_name enquo
+#' @importFrom dplyr filter
+#' @importFrom ggplot2 aes
+add_partial_roc_from_predictor_fpr <- function(
+        data,
+        response,
+        predictor,
+        threshold,
+        geom) {
+    predictor_name <- as_name(enquo(predictor))
+    if (is.null(data)) {
+        geom(
+            data = . %>%
+                roc_points({{ response }}, {{ predictor }}) %>%
+                filter(.data[["fpr"]] <= threshold),
+            mapping = aes(
+                x = .data[["fpr"]],
+                y = .data[["tpr"]],
+                color = predictor_name
+            ),
+            size = 0.8
+        )
+    } else {
+        geom(
+            data = roc_points(data, {{ response }}, {{ predictor }}) %>%
+                filter(.data[["fpr"]] <= threshold),
+            mapping = aes(
+                x = .data[["fpr"]],
+                y = .data[["tpr"]],
+                color = predictor_name
+            ),
+            size = 0.8
+        )
+    }
+}
+
+add_partial_roc_from_predictor <- function(
+        data,
+        response,
+        predictor,
+        ratio,
+        threshold,
+        geom) {
+    if (ratio == "tpr") {
+        add_partial_roc_from_predictor_tpr(
+            data,
+            {{ response }},
+            {{ predictor }},
+            threshold,
+            geom
+        )
+    } else if (ratio == "fpr") {
+        add_partial_roc_from_predictor_fpr(
+            data,
+            {{ response }},
+            {{ predictor }},
+            threshold,
+            geom
+        )
+    }
+
+}
+
+#' @importFrom rlang quo_is_null enquo
 add_partial_roc <- function(
         data,
         fpr,
         tpr,
         response,
         predictor,
-        ratio = NULL,
-        threshold) {
-
+        ratio,
+        threshold,
+        geom) {
+    predictor_expr <- enquo(predictor)
+    response_expr <- enquo(response)
+    if (!quo_is_null(predictor_expr) & !quo_is_null(response_expr)) {
+        add_partial_roc_from_predictor(
+            data,
+            {{ response }},
+            {{ predictor }},
+            ratio,
+            threshold,
+            geom
+        )
+    } else {
+        add_partial_roc_from_ratios(
+            data,
+            {{ fpr }},
+            {{ tpr }},
+            ratio,
+            threshold,
+            geom
+        )
+    }
 }
+
+#' @importFrom ggplot2 geom_path
+add_partial_roc_curve <- function(
+        data = NULL,
+        fpr = NULL,
+        tpr = NULL,
+        response = NULL,
+        predictor = NULL,
+        ratio,
+        threshold) {
+    add_partial_roc(
+        data,
+        {{ fpr }},
+        {{ tpr }},
+        {{ response }},
+        {{ predictor }},
+        ratio,
+        threshold,
+        geom_path
+    )
+}
+
+#' @importFrom ggplot2 geom_point
+add_partial_roc_points <- function(
+        data = NULL,
+        fpr = NULL,
+        tpr = NULL,
+        response = NULL,
+        predictor = NULL,
+        ratio,
+        threshold) {
+    add_partial_roc(
+        data,
+        {{ fpr }},
+        {{ tpr }},
+        {{ response }},
+        {{ predictor }},
+        ratio,
+        threshold,
+        geom_point
+    )
+}
+
+
+# DELETE functions --------------------------------------------------------
 
 #' @importFrom ggplot2 geom_path
 #' @importFrom dplyr filter
