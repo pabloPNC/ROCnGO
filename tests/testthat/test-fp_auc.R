1112ef6efd3f1a2c7a0d5626e94d9b38c2c44128
pabloPNC <pablo.navarrocarpio@gmail.com>
2025-02-18 21:45:05 +0100

    Included test files for NpAUC and FpAUC

---
+++ tests/testthat/test-fp_auc.R
@@ -0,0 +1,27 @@
+data <- tibble::tibble(readRDS(test_path("fixtures", "roc_data.rds")))
+response <- "disease"
+predictor <- "ENSG00000000003.15"
+
+test_that("fp_auc == FpaucHS", {
+    lower_threshold <- 1
+
+    actual_fpauc <- suppressWarnings(
+        fp_auc(
+            data = NULL,
+            response = data[[response]],
+            predictor = data[[predictor]],
+            lower_tpr = 0.9
+        )
+    )
+
+    expected_fpauc <- FpaucHS(
+        xsample = data[[response]],
+        ysample = data[[predictor]],
+        lower.sen = 0.9
+    )
+
+    expect_equal(
+        actual_fpauc,
+        expected_fpauc
+    )
+})
