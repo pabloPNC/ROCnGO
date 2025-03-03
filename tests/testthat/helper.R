25b03fd13c7e9474de47c28687d630886abcfe8e
pabloPNC <pablo.navarrocarpio@gmail.com>
2025-03-03 11:39:12 +0100

    Added expected virg bin_fct var to test df

--- tests/testthat/helper.R
+++ tests/testthat/helper.R
@@ -3,19 +3,25 @@ create_iris_df <- function() {
   iris_df <- tibble::as_tibble(iris)
   # Add test variables
   iris_df <- iris_df %>% dplyr::mutate(
-    Species_chr = as.character(Species),
-    Species_int = as.integer(Species),
+    Species_chr = as.character(.data$Species),
+    Species_int = as.integer(.data$Species),
     Species_bin_fct = forcats::fct_recode(
-      Species,
+      .data$Species,
       "0" = "virginica",
       "0" = "versicolor",
       "1" = "setosa"
     ) %>% forcats::fct_relevel("0")
-  ) %>% dplyr::mutate(
+  ) %>%
+    dplyr::mutate(
       Species_bin_fct_inv = forcats::fct_recode(
-        Species_bin_fct,
+        .data$Species_bin_fct,
         "0" = "1",
         "1" = "0"
       ) %>% forcats::fct_relevel("0")
-  )
+    ) %>%
+    dplyr::mutate(
+      Species_bin_fct_virg = .data$Species %>%
+        fct_collapse("0" = c("setosa", "versicolor"), "1" = "virginica") %>%
+        fct_relevel("0", "1")
+    )
 }
