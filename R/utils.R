17e42a5079a2e56785792e6909cd66fde6815257
pabloPNC <pablo.navarrocarpio@gmail.com>
2025-03-10 19:13:58 +0100

    Included message capturing function

--- R/utils.R
+++ R/utils.R
@@ -11,3 +11,20 @@ mask_name <- function(expr) {
     as_name(expr)
   }
 }
+
+capture_messages <- function(expr) {
+  messages <- list()
+
+  result <- withCallingHandlers(
+    expr,
+    message = function(cnd) {
+      messages <<- c(messages, list(cnd))
+      invokeRestart("muffleMessage")
+    }
+  )
+
+  list(
+    result = result,
+    messages = messages
+  )
+}
