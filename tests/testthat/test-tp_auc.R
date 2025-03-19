37fb8e69c9f4aff4b886f3ef37ff715cbbf585d8
pabloPNC <pablo.navarrocarpio@gmail.com>
2025-03-19 22:53:59 +0100

    TpAUC calc test dep removed

--- tests/testthat/test-tp_auc.R
+++ tests/testthat/test-tp_auc.R
@@ -2,29 +2,24 @@ data <- tibble::tibble(readRDS(test_path("fixtures", "roc_data.rds")))
 response <- "disease"
 predictor <- "ENSG00000000003.15"
 
-test_that("tp_auc == TpAUC.function", {
-  lower_threshold <- 0.4
-  upper_threshold <- 0.49
-
-  actual_tpauc <- tp_auc(
-    data = NULL,
-    response = data[[response]],
-    predictor = data[[predictor]],
-    lower_fpr = lower_threshold,
-    upper_fpr = upper_threshold
+test_that("tp_auc is correct", {
+  test_iris <- create_iris_df()
+  tpauc <- suppressMessages(
+    tp_auc(
+      data = test_iris,
+      response = Species_bin_fct,
+      predictor = Sepal.Width,
+      lower_fpr = 0,
+      upper_fpr = 0.5
+    )
   )
-
   expected_tpauc <- TpAUC.function(
-    data[[response]],
-    data[[predictor]],
-    lower.fp = lower_threshold,
-    upper.fp = upper_threshold
+    test_iris[["Species_bin_fct"]],
+    test_iris[["Sepal.Width"]],
+    lower.fp = 0,
+    upper.fp = 0.5
   )
-
-  expect_equal(
-    actual_tpauc,
-    expected_tpauc
-  )
+  expect_equal(tpauc, expected_tpauc)
 })
 
 test_that("tp_auc faster than TpAUC.function", {
