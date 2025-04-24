4d1e585c16aee646ec9749766b7a4e5367994ee3
pabloPNC <pablo.navarrocarpio@gmail.com>
2025-04-24 17:46:04 +0200

    Fixed concordance indexes arguments

--- R/concordace.R
+++ R/concordace.R
@@ -2,17 +2,18 @@
 cp_auc <- function(data = NULL,
                    response,
                    predictor,
-                   lower_fpr,
-                   upper_fpr,
+                   lower_threshold,
+                   upper_threshold,
+                   ratio,
                    .condition = NULL) {
   ppoints <- calc_partial_roc_points(
     data = data,
     response = {{ response }},
     predictor = {{ predictor }},
     .condition = {{ .condition }},
-    lower_threshold = lower_fpr,
-    upper_threshold = upper_fpr,
-    ratio = "fpr"
+    lower_threshold = lower_threshold,
+    upper_threshold = upper_threshold,
+    ratio = ratio
   )
   pauc_fpr <- pauc_fpr(ppoints$partial_fpr, ppoints$partial_tpr)
   pauc_tpr <- pauc_tpr(ppoints$partial_fpr, ppoints$partial_tpr)
@@ -24,25 +25,27 @@ cp_auc <- function(data = NULL,
 ncp_auc <- function(data = NULL,
                     response,
                     predictor,
-                    lower_fpr,
-                    upper_fpr,
+                    lower_threshold,
+                    upper_threshold,
+                    ratio,
                     .condition = NULL) {
   c_pauc <- cp_auc(
     data = data,
     response = {{ response }},
     predictor = {{ predictor }},
     .condition = {{ .condition }},
-    lower_fpr = lower_fpr,
-    upper_fpr = upper_fpr
+    lower_threshold = lower_threshold,
+    upper_threshold = upper_threshold,
+    ratio = ratio
   )
   ppoints <- calc_partial_roc_points(
     data = data,
     response = {{ response }},
     predictor = {{ predictor }},
     .condition = {{ .condition }},
-    lower_threshold = lower_fpr,
-    upper_threshold = upper_fpr,
-    ratio = "fpr"
+    lower_threshold = lower_threshold,
+    upper_threshold = upper_threshold,
+    ratio = ratio
   )
   fpr_range <- ppoints$partial_fpr[nrow(ppoints)] - ppoints$partial_fpr[1]
   tpr_range <- ppoints$partial_tpr[nrow(ppoints)] - ppoints$partial_tpr[1]
