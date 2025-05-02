a8d6d7e260686c08a2376275aa475a9bbe3d1a2b
pabloPNC <pablo.navarrocarpio@gmail.com>
2025-05-02 23:57:23 +0200

    Included temp_functions to test

--- R/auc.R
+++ R/auc.R
@@ -26,7 +26,13 @@ temp_auc_tpauc <- function(.data,
                            response,
                            predictor,
                            .condition = NULL) {
-  df <- .data %>% dplyr::relocate({{ response }})
+  df <- .data %>%
+    dplyr::select({{ response }}, {{ predictor }}) %>%
+    mutate(
+      {{ response }} := as.integer(
+        transform_response({{ response }}, .condition)
+      )
+    )
   sumexp <- ROCpAI::mcpAUC(df, low.value = 0, up.value = 1, plot = FALSE)
   SummarizedExperiment::assay(sumexp)
 }
@@ -48,10 +54,18 @@ temp_auc_proc <- function(.data,
   )
 }
 
-# TODO: prepare function to compare != options
 temp_comparison <- function(.data,
                             response,
                             predictor,
-                            .condition) {
-  
+                            .condition = NULL) {
+  auc <- temp_auc(.data, {{ response }}, {{ predictor }}, .condition)
+  auc_proc <- temp_auc_proc(
+    .data, {{ response }}, {{ predictor }}, .condition
+  )
+  auc_rocpai <- temp_auc_tpauc(
+    .data, {{ response }}, {{ predictor }}, .condition
+  )
+  print(auc)
+  print(auc_proc)
+  print(auc_rocpai$pAUC)
 }
