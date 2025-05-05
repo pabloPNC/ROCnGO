1feb05532e0d663d56bb19812f1c79a0421aa117
pabloPNC <pablo.navarrocarpio@gmail.com>
2025-05-05 06:45:51 +0200

    Included auc function

--- R/auc.R
+++ R/auc.R
@@ -1,3 +1,15 @@
+#' @title Calculate area under ROC curve
+#' @description
+#' Calculates area under curve (AUC) of a predictor's ROC curve.
+#' @inheritParams roc_points
+#' @returns
+#' A numerical value representing the area under ROC curve.
+#' @examples
+#' # Calc AUC of Sepal.Width as a classifier of setosa species
+#' auc(iris, Species, Sepal.Width)
+#' # Change class to predict to virginica
+#' auc(iris, Species, Sepal.Width, .condition = "virginica")
+#' @export
 auc <- function(.data = NULL,
                 response,
                 predictor,
