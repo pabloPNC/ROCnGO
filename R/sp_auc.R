11546a44981bc9da842168aa8ef89439ee237955
pabloPNC <pablo.navarrocarpio@gmail.com>
2025-05-09 05:05:35 +0200

    SpAUC convertion to S3

--- R/sp_auc.R
+++ R/sp_auc.R
@@ -7,35 +7,32 @@
 #' @references
 #' McClish D. K. Analyzing a Portion of the ROC Curve. *Medical Decision Making*
 #' 9, 190-195 (1989).
-sp_auc <- function(
-    data = NULL,
-    response,
-    predictor,
-    lower_fpr,
-    upper_fpr,
-    .condition = NULL,
-    .invalid = FALSE) {
-  if (!is.null(data)) {
-    tpr_fpr <- data %>% roc_points({{ response }}, {{ predictor }}, .condition)
-    tpr <- tpr_fpr %>% pull(tpr)
-    fpr <- tpr_fpr %>% pull(fpr)
-  } else {
-    tpr_fpr <- NULL %>% roc_points(response, predictor, .condition)
-    tpr <- tpr_fpr[["tpr"]]
-    fpr <- tpr_fpr[["fpr"]]
-  }
-  partial_tpr_fpr <- calc_partial_roc_points(
-    tpr = tpr,
-    fpr = fpr,
-    lower_threshold = lower_fpr,
-    upper_threshold = upper_fpr,
-    ratio = "fpr"
+sp_auc <- function(data,
+                   response,
+                   predictor,
+                   lower_fpr,
+                   upper_fpr,
+                   .condition = NULL,
+                   .invalid = FALSE) {
+  UseMethod("sp_auc", data)
+}
+
+#' @export
+sp_auc.ratio_df <- function(data,
+                            response,
+                            predictor,
+                            lower_fpr,
+                            upper_fpr,
+                            .condition = NULL,
+                            .invalid = FALSE) {
+  # fpr, tpr usage
+  cli::cli_alert("In {.cls ratio_df}")
+  pauc <- pauc_fpr(data$fpr, data$tpr)
+  lower_bound <- calc_fpr_diagonal_lower_bound(
+    data$fpr,
+    data$tpr
   )
-  partial_tpr <- partial_tpr_fpr[["partial_tpr"]]
-  partial_fpr <- partial_tpr_fpr[["partial_fpr"]]
-  pauc <- pauc_fpr(partial_fpr, partial_tpr)
-  lower_bound <- calc_fpr_diagonal_lower_bound(partial_fpr, partial_tpr)
-  upper_bound <- upper_fpr - lower_fpr
+  upper_bound <- max(data$fpr) - min(data$fpr)
 
   if (pauc < lower_bound && !.invalid) {
     # TODO: improve warning when pauc < under diagonal
@@ -45,4 +42,48 @@ sp_auc <- function(
     spauc <- (1 + ((pauc - lower_bound) / (upper_bound - lower_bound))) / 2
   }
   spauc
+}
+
+#' @export
+sp_auc.NULL <- function(data,
+                        response,
+                        predictor,
+                        lower_fpr,
+                        upper_fpr,
+                        .condition = NULL,
+                        .invalid = FALSE) {
+  # vector usage
+  cli::cli_alert("In {.cls NULL}")
+  ratios <- roc_points(NULL, response, predictor, .condition)
+  pratios <- calc_partial_roc_points(
+    tpr = ratios$tpr,
+    fpr = ratios$fpr,
+    lower_threshold = lower_fpr,
+    upper_threshold = upper_fpr,
+    ratio = "fpr"
+  )
+  sp_auc.ratio_df(pratios, .condition = .condition, .invalid = .invalid)
+}
+
+#' @export
+sp_auc.data.frame <- function(data,
+                              response,
+                              predictor,
+                              lower_fpr,
+                              upper_fpr,
+                              .condition = NULL,
+                              .invalid = FALSE) {
+  # df  usage
+  cli::cli_alert("In {.cls data.frame}")
+  response <- pull(data, {{ response }})
+  predictor <- pull(data, {{ predictor }})
+  sp_auc.NULL(
+    NULL,
+    response,
+    predictor,
+    lower_fpr,
+    upper_fpr,
+    .condition,
+    .invalid
+  )
 }
