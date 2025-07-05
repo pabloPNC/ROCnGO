77939dfa30223bda296a5bf04340ccd88121d9c1
pabloPNC <pablo.navarrocarpio@gmail.com>
2025-07-05 19:25:07 +0200

    Silencing warnings/messages in tests

--- tests/testthat/test-bounds.R
+++ tests/testthat/test-bounds.R
@@ -1,13 +1,15 @@
 test_that("FPR diagonal_lower_bound is correct", {
   test_iris <- create_iris_df()
   partial_points <- suppressMessages(
-    calc_partial_roc_points(
-      data = test_iris,
-      response = Species_bin_fct,
-      predictor = Sepal.Width,
-      lower_threshold = 0,
-      upper_threshold = 0.5,
-      ratio = "fpr"
+    suppressWarnings(
+      calc_partial_roc_points(
+        data = test_iris,
+        response = Species_bin_fct,
+        predictor = Sepal.Width,
+        lower_threshold = 0,
+        upper_threshold = 0.5,
+        ratio = "fpr"
+      )
     )
   )
   bound <- calc_fpr_diagonal_lower_bound(
@@ -26,13 +28,15 @@ test_that("FPR diagonal_lower_bound is correct", {
 test_that("FPR rectangle lower bound is correct", {
   test_iris <- create_iris_df()
   partial_points <- suppressMessages(
-    calc_partial_roc_points(
-      data = test_iris,
-      response = Species_bin_fct,
-      predictor = Sepal.Width,
-      lower_threshold = 0,
-      upper_threshold = 0.5,
-      ratio = "fpr"
+    suppressWarnings(
+      calc_partial_roc_points(
+        data = test_iris,
+        response = Species_bin_fct,
+        predictor = Sepal.Width,
+        lower_threshold = 0,
+        upper_threshold = 0.5,
+        ratio = "fpr"
+      )
     )
   )
   bound <- calc_fpr_square_lower_bound(
@@ -51,13 +55,15 @@ test_that("FPR rectangle lower bound is correct", {
 test_that("FPR proper lower bound is correct", {
   test_iris <- create_iris_df()
   partial_points <- suppressMessages(
-    calc_partial_roc_points(
-      data = test_iris,
-      response = Species_bin_fct,
-      predictor = Sepal.Width,
-      lower_threshold = 0,
-      upper_threshold = 0.5,
-      ratio = "fpr"
+    suppressWarnings(
+      calc_partial_roc_points(
+        data = test_iris,
+        response = Species_bin_fct,
+        predictor = Sepal.Width,
+        lower_threshold = 0,
+        upper_threshold = 0.5,
+        ratio = "fpr"
+      )
     )
   )
   bound <- calc_fpr_proper_lower_bound(
@@ -76,13 +82,15 @@ test_that("FPR proper lower bound is correct", {
 test_that("FPR PLR/Concave lower bound is correct", {
   test_iris <- create_iris_df()
   partial_points <- suppressMessages(
-    calc_partial_roc_points(
-      data = test_iris,
-      response = Species_bin_fct,
-      predictor = Sepal.Width,
-      lower_threshold = 0,
-      upper_threshold = 0.5,
-      ratio = "fpr"
+    suppressWarnings(
+      calc_partial_roc_points(
+        data = test_iris,
+        response = Species_bin_fct,
+        predictor = Sepal.Width,
+        lower_threshold = 0,
+        upper_threshold = 0.5,
+        ratio = "fpr"
+      )
     )
   )
   bound <- calc_fpr_plr_lower_bound(
@@ -101,13 +109,15 @@ test_that("FPR PLR/Concave lower bound is correct", {
 test_that("FPR lower bound is correctly selected", {
   test_iris <- create_iris_df()
   partial_points <- suppressMessages(
-    calc_partial_roc_points(
-      data = test_iris,
-      response = Species_bin_fct,
-      predictor = Sepal.Width,
-      lower_threshold = 0,
-      upper_threshold = 0.5,
-      ratio = "fpr"
+    suppressWarnings(
+      calc_partial_roc_points(
+        data = test_iris,
+        response = Species_bin_fct,
+        predictor = Sepal.Width,
+        lower_threshold = 0,
+        upper_threshold = 0.5,
+        ratio = "fpr"
+      )
     )
   )
   bound <- calc_fpr_lower_bound(
@@ -126,13 +136,15 @@ test_that("FPR lower bound is correctly selected", {
 test_that("FPR upper bound is correct", {
   test_iris <- create_iris_df()
   partial_points <- suppressMessages(
-    calc_partial_roc_points(
-      data = test_iris,
-      response = Species_bin_fct,
-      predictor = Sepal.Width,
-      lower_threshold = 0,
-      upper_threshold = 0.5,
-      ratio = "fpr"
+    suppressWarnings(
+      calc_partial_roc_points(
+        data = test_iris,
+        response = Species_bin_fct,
+        predictor = Sepal.Width,
+        lower_threshold = 0,
+        upper_threshold = 0.5,
+        ratio = "fpr"
+      )
     )
   )
   bound <- calc_fpr_upper_bound(
@@ -151,13 +163,15 @@ test_that("FPR upper bound is correct", {
 test_that("FPR both bounds are correct", {
   test_iris <- create_iris_df()
   partial_points <- suppressMessages(
-    calc_partial_roc_points(
-      data = test_iris,
-      response = Species_bin_fct,
-      predictor = Sepal.Width,
-      lower_threshold = 0,
-      upper_threshold = 0.5,
-      ratio = "fpr"
+    suppressWarnings(
+      calc_partial_roc_points(
+        data = test_iris,
+        response = Species_bin_fct,
+        predictor = Sepal.Width,
+        lower_threshold = 0,
+        upper_threshold = 0.5,
+        ratio = "fpr"
+      )
     )
   )
   bounds <- calc_fpr_bounds(
@@ -177,24 +191,28 @@ test_that("FPR both bounds are correct", {
 test_that("curve_shape works with .condition", {
   test_iris <- create_iris_df()
   curve_shape <- suppressMessages(
-    calc_curve_shape(
-      data = test_iris,
-      response = Species,
-      predictor = Sepal.Length,
-      lower_threshold = 0,
-      upper_threshold = 0.5,
-      ratio = "tpr",
-      .condition = "virginica"
+    suppressWarnings(
+      calc_curve_shape(
+        data = test_iris,
+        response = Species,
+        predictor = Sepal.Length,
+        lower_threshold = 0,
+        upper_threshold = 0.5,
+        ratio = "tpr",
+        .condition = "virginica"
+      )
     )
   )
   expected_curve_shape <- suppressMessages(
-    calc_curve_shape(
-      data = test_iris,
-      response = Species_bin_fct_virg,
-      predictor = Sepal.Length,
-      lower_threshold = 0,
-      upper_threshold = 0.5,
-      ratio = "tpr"
+    suppressWarnings(
+      calc_curve_shape(
+        data = test_iris,
+        response = Species_bin_fct_virg,
+        predictor = Sepal.Length,
+        lower_threshold = 0,
+        upper_threshold = 0.5,
+        ratio = "tpr"
+      )
     )
   )
   expect_equal(curve_shape, expected_curve_shape)
